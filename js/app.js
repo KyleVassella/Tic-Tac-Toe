549e128fdba49c898db226e58d0f88861f84e088
Kyle Vassella <kylevassella@gmail.com>
2017-03-07 13:12:33 -0600

    Selected square bg colors, name size increase, results delay,  other visuals.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 //Change class names around to meet requirements
+//Don't clear the name values each time. Only replace if a new value is given. 
+//Have a 'swap' button for player 1 and player 2 after the game
+//Display the names in the inputs 
 //Do you really need to change name every time? Well, high probability people will alternate between who goes first. So if you include it, state that its optional and only remove names if the new input is given value. 
 
 "use strict";
@@ -42,7 +45,7 @@ $('.button').click(function(){
 	$('#win-two').hide();
 	$('#tie').hide();
 	$box.removeClass('x-clicked o-clicked clicked');
-	$box.css('background-image', 'none');
+	$box.css({'background-image':'none','background-color':'#EFEFEF'});
 	$player1.addClass('active');
 	$player2.removeClass('active');
 	$('.player-1-name-display').empty();
@@ -63,35 +66,43 @@ $('.button').click(function(){
 $box.click(function(){
 	var self = $(this);
 	if ($('#player1').hasClass("active") && !self.hasClass('x-clicked') && !self.hasClass('o-clicked') && !self.hasClass('clicked') ) {			// second condition prevents multiple svg being appended to same <li> // why does 'this' (without attaching it to a variable) result in text being appended and not the svg?
+		
 		$player1.toggleClass("active");
 		$player2.toggleClass("active");
-		self.css("background-image", "(url:'media/o.svg')");
+		self.css({"background-image":"(url:'media/o.svg')", "background-color":"#ffa000"});
 		self.addClass("o-clicked");
 		self.addClass("clicked");
 	} else if ($('#player2').hasClass("active") && !self.hasClass('x-clicked') && !self.hasClass('o-clicked') && !self.hasClass('clicked') ) {
 		$player1.toggleClass("active");
 		$player2.toggleClass("active");
-		self.css("background-image", "url('media/x.svg')");
+		self.css({"background-image":"url('media/x.svg')","background-color":"#3b89c1"});
 		self.addClass("x-clicked");
 		self.addClass("clicked");
+		
 	}
 	if (isClickedO([0, 1, 2]) || isClickedO([3, 4, 5]) || isClickedO([6, 7, 8]) || isClickedO([0, 3, 6]) || isClickedO([1, 4, 7]) || isClickedO([2, 5, 8]) || isClickedO([0, 4, 8]) || isClickedO([2, 4, 6])) {
-			$('.winner-name').append(current1Input);
-			current1Input = '';		// this clears the current turn name, not the winner name
-			current2Input = '';
-			clearInputs();			// clears the winner name, not the turn name. At this point both are needed with the way your code is written.
-			$('#win-one').show();
+			setTimeout(function(){
+				$('.winner-name').append(current1Input);
+				current1Input = '';		// this clears the current turn name, not the winner name
+				current2Input = '';
+				clearInputs();			// clears the winner name, not the turn name. At this point both are needed with the way your code is written.
+				$('#win-one').show();
+			}, 135);
 		} else if (isClickedX([0, 1, 2]) || isClickedX([3, 4, 5]) || isClickedX([6, 7, 8]) || isClickedX([0, 3, 6]) || isClickedX([1, 4, 7]) || isClickedX([2, 5, 8]) || isClickedX([0, 4, 8]) || isClickedX([2, 4, 6])) {
-			$('.winner-name').append(current2Input);
-			current1Input = '';
-			current2Input = '';
-			clearInputs();	
-			$('#win-two').show();
+			setTimeout(function(){
+				$('.winner-name').append(current2Input);
+				current1Input = '';
+				current2Input = '';
+				clearInputs();	
+				$('#win-two').show();
+			}, 135);
 		} else if(isClicked([0, 1, 2, 3, 4, 5, 6, 7, 8])){
-			current1Input = '';
-			current2Input = '';
-			clearInputs();	
-			$('#tie').show();
+			setTimeout(function(){
+				current1Input = '';
+				current2Input = '';
+				clearInputs();	
+				$('#tie').show();
+			}, 135);
 		}
 });
 
